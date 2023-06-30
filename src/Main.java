a1c6a934c08aeb979632653946528ff8a441de33
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-06-30 11:29:25 -0500

    main finished

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        //objeto1
+        Pais pais1 = new Pais("España", "Europa");
+        //objeto2
+        Provincia prov1 = new Provincia("Ecuador", "America", "Pichincha");
+        //objeto 3
+        Ciudad ciu1 = new Ciudad("España", "Europa", "Andalucia", "Granada");
+        //objeto 4
+        Barrio bar1 = new Barrio("Ecuador", "America", "Cuenca", "Cuenca capital", "Jaime Roldos", 4000);
     }
+
 }
